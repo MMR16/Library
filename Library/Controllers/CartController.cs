d94c4c6dc5e0a43ce0a26a9ccd039f867b92b626
Mostafa Mahmoud <mmr16rady@yahoo.com>
2021-03-07 14:05:41 +0200

    adding cart view

---
+++ Library/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using Library.Data;
+using Library.Models;
+using Library.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CartController : Controller
+    {
+        readonly ApplicationDBContext db;
+        public CartController(ApplicationDBContext _db)
+        {
+            db = _db;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ShoppingCart> ShoppingCartList = new List<ShoppingCart>();
+            var session = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessinCart);
+            //list
+            //var session = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessinCart);
+            if (session is not null && session.Count()>0)
+            {
+                //expilicit converting to list
+                ShoppingCartList = session.ToList();
+
+            }
+            List<int?> porductInCart = ShoppingCartList.Select(q => q.ProductId).ToList();
+            IEnumerable<Product> productList = db.Products.Where(q => porductInCart.Contains(q.ProId));
+            return View(productList);
+        }
+    }
+}
